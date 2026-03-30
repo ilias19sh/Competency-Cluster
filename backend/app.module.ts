d55529d4eed30a05fac714e1c1f4b0cfc97035bf
Ilias HANFAOUI <ilias.hanfaoui@decathlon.com>
2026-03-30 15:26:04 +0200

    chore(XXXX): test prod database

--- backend/app.module.ts
+++ backend/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './src/features/users/users.module';
+import { AppController } from 'app.controller';
+import { PrismaService } from 'src/api';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [],
-  providers: [],
+  imports: [/* UsersModule */],
+  controllers: [AppController],
+  providers: [PrismaService],
 })
 export class AppModule {}
