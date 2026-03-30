1efc224f73edd285277a7634bff3b7749a401271
Ilias HANFAOUI <ilias.hanfaoui@decathlon.com>
2026-03-30 16:18:44 +0200

    chore(XXXX): test prod database

--- backend/prisma.service.ts
+++ backend/prisma.service.ts
@@ -3,20 +3,14 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  // On ne met pas de constructeur personnalisé pour laisser Prisma
-  // s'initialiser tranquillement avec les variables d'env par défaut.
-
   async onModuleInit() {
-    console.log('Tentative de connexion à la BDD...');
-    // On ne met pas de "await" pour ne pas bloquer NestJS si la BDD est lente
-    this.$connect()
-      .then(() => {
-        console.log('✅ CONNEXION BDD RÉUSSIE');
-      })
-      .catch((err) => {
-        // Ici, on log l'erreur mais on ne "throw" pas, pour que l'app reste en vie
-        console.error('❌ ÉCHEC CONNEXION BDD (mais l’app tourne) :', err.message);
-      });
+    try {
+      await this.$connect();
+      console.log('✅ CONNEXION BDD RÉUSSIE');
+    } catch (err) {
+      console.error('❌ ÉCHEC CONNEXION BDD :', err.message);
+      throw err;
+    }
   }
 
   async onModuleDestroy() {
