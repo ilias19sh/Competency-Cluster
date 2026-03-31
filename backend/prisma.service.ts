254020d5837f4b2499b32c6494b502408b334438
Ilias HANFAOUI <ilias.hanfaoui@decathlon.com>
2026-03-31 08:52:29 +0200

    chore(XXXX): test prod database

--- backend/prisma.service.ts
+++ backend/prisma.service.ts
@@ -4,17 +4,17 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    const url = process.env.DATABASE_URL;
-    
     super({
-      datasourceUrl: url,
-    } as any); 
+      datasource: {
+        url: process.env.DATABASE_URL,
+      },
+    } as any);
   }
 
   async onModuleInit() {
     this.$connect()
       .then(() => console.log('✅ PRISMA V7 CONNECTÉ'))
-      .catch((err) => console.error('❌ ERREUR CONNEXION V7:', err.message));
+      .catch((err) => console.error('❌ PRISMA V7 ERROR:', err.message));
   }
 
   async onModuleDestroy() {
