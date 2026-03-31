9f7dcf1aa2731150bd472d5829ca7f29ccdd60a2
Ilias HANFAOUI <ilias.hanfaoui@decathlon.com>
2026-03-31 16:31:30 +0200

    chore(XXXX): test prod database

--- backend/app.module.ts
+++ backend/app.module.ts
@@ -6,6 +6,6 @@ import { PrismaService } from './prisma.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
