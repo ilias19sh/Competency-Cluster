edd0e19039e872edd15e8961894f864a95237454
mac-IHANFA18 <hanfaouiilias4@gmail.com>
2026-05-14 11:10:26 -0400

    fix(CC-XXXX): fix cors errors

--- backend/main.ts
+++ backend/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',
+    'https://www.competency-cluster.fr',
+    'https://competency-cluster.fr'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
